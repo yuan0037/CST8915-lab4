8ea1285597bd268e1263347cf2d3875d70a48b7e
Bo Yuan <boyuxa@syntronic.com>
2023-10-10 12:13:00 -0400

    updated comments list.

--- my_blog/services/comment_service/app.py
+++ my_blog/services/comment_service/app.py
@@ -5,8 +5,8 @@ import requests
 
 app = Flask(__name__)
 comments = [
-    {'id': 1, 'user_id': '1', 'post_id': '2', 'comment': 'Amazing comment!'},
-    {'id': 2, 'user_id': '2', 'post_id': '2', 'comment': 'I did not know that'}
+    {'id': 1, 'user_id': 1, 'post_id': 2, 'comment': 'Amazing comment!'},
+    {'id': 2, 'user_id': 2, 'post_id': 2, 'comment': 'I did not know that'}
 ]
 
 @app.route('/')
